549bbcc75b8070173d163790fe6dc3bc6d7e4b19
tina1998612 <tleeae@connect.ust.hk>
2022-03-02 16:44:20 +0800

    first commit

---
+++ front.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT License
+pragma solidity >=0.7.0 <0.9.0;
+
+contract The Z Institute {
+
+  string name = "Lee Ting Ting";
+  string title = "Founder & CEO";
+  string email = "tina@zinstitute.net";
+  address buyMeCoffee = 0xe5BAb6568a3a1035f0aeF619e28EE7D3A78cAe05;
+
+  uint256 power;
+  function consult() public payable {
+    power += msg.value * uint (bytes32 ("DApps Development x Education")) ;
+  }
+}
